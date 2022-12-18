35e1b18173515a8f23dacaeb1e37431657b1936f
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-12-18 21:15:45 +0530

    banner done

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
-import Carousel from 'react-multi-carousel'
+import React from "react";
+import Carousel from "react-multi-carousel";
+import {styled} from "@mui/material"
+import { bannerData } from "../../constants/data";
+import 'react-multi-carousel/lib/styles.css'
 
+const Image = styled('img')({
+    height:'40vh',
+    width:'100vw'
+})
 const responsive = {
-    superLargeDesktop:{
-        breakpoint:{max:4000,min:3000},
-        items:5
-    },
-    desktop:{
-        breakpoint:{max:3000,min:1024},
-        items:3
-    },
-    tablet:{
-        breakpoint:{max:1024,min:464},
-        items:2
-    },
-    mobile:{
-        breakpoint:{max:464,min:0},
-        item:1
-    }
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    item: 1,
+  },
 };
 const Banner = () => {
   return (
     <>
-    <Carousel responsive={responsive}>21</Carousel>
+      <Carousel 
+      responsive={responsive}
+      swipeable={false}
+      draggable={false}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={5000}
+      dotListClass='custom-dot-list-style'
+      itemClass="carousel-item-padding-40-px"
+      containerClass="carousel-container"
+      >
+        {bannerData.map((data) => (
+          <Image src={data.url} alt="banner" />
+        ))}
+      </Carousel>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
 //components
-import NavBar from './NavBar'
-import Banner from './Banner'
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+
+//styled
+import { Box, styled } from "@mui/material";
+
+const Component = styled(Box)`
+  padding: 10px;
+  background: #f2f2f2;
+`;
 const Home = () => {
   return (
     <>
-    <NavBar />
-    <Banner />
+      <NavBar />
+      <Component>
+        <Banner />
+      </Component>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
