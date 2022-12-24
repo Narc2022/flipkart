46c28e1d8cdb1d83f9ab0e2edcef51859da1531e
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-12-24 16:34:29 +0530

    test repo

--- server/index.js
+++ server/index.js
@@ -4,4 +4,5 @@ const app = express();
 
 
 const PORT = 8000;
-app.listen(PORT,()=>console.log(`server is running successfully on PORT ${PORT}`));
+
+app.listen(PORT,()=>console.log(`server is running successfully on PORT ${PORT}`)); 
