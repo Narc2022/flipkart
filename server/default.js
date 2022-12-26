89b6966e7d73448cbdbc11768c29aea3d341f006
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-12-26 09:54:33 +0530

    server small part created now loging ui starts

--- server/default.js
+++ server/default.js
@@ -1,11 +1,15 @@
 
-import { products } from "./constants/data"
+import { products } from "./constants/data.js"
+import Product from "./model/product-schema.js";
 
-
-const DefaultData = () =>{
+const DefaultData = async() =>{
    try{
-      
-   }catch(erroe){
+      // await Product.deleteMany({});
+      await Product.insertMany(products)
+      console.log('data imported successfully');
+   }catch(error){
       console.log('Error while inserting default data',error.message);
    }
 }
+
+export default DefaultData;
--- server/model/product-schema.js
+++ server/model/product-schema.js
@@ -1,2 +1,21 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
+ const productSchema = new mongoose.Schema({
+    id: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    url:String,
+    detailUrl:String,
+    title:Object,
+    price:Object,
+    quality:Number,
+    description:String,
+    discount:String,
+    tagline:String
+ });
+
+const Product = mongoose.model('product',productSchema);
+
+export default Product;
